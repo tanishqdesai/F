7e715fc9eea696fc57064360881601d3b7a15869
tanishqdesai <145229073+tanishqdesai@users.noreply.github.com>
2025-12-09 10:23:10 -1100

    Update User.js with username email password and date along with their correct datatype assignment.

--- User.js
+++ User.js
@@ -1,1 +1,22 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
