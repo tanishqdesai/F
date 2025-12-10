2c306402f62fe1440111294abffa9e652dfaf7f3
tanishqdesai <145229073+tanishqdesai@users.noreply.github.com>
2025-12-09 21:20:06 -1100

    Create File.js

---
+++ File.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const FileSchema = new mongoose.Schema({
+    filename: {
+        type: String,
+        required: true
+    },
+    originalName: {
+        type: String,
+        required: true
+    },
+    path: {
+        type: String,
+        required: true
+    },
