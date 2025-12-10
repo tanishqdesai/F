2d201decb4d4086c1255a8d1cb07787bd1e16a0c
tanishqdesai <145229073+tanishqdesai@users.noreply.github.com>
2025-12-09 21:14:57 -1100

    Code correction passwd checking update comparison module exp

--- User.js
+++ User.js
@@ -20,16 +20,18 @@ const UserSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
-    // Hash password to before save it
-    UserSchema.pre('save', async function(next) {
+});
+
+// Hash password before saving
+UserSchema.pre('save', async function(next) {
     if (!this.isModified('password')) return next();
     this.password = await bcrypt.hash(this.password, 10);
     next();
-    });
+});
 
-    // Compare password method
-    UserSchema.methods.comparePassword = async function(password) {
+// Compare password method
+UserSchema.methods.comparePassword = async function(password) {
     return await bcrypt.compare(password, this.password);
 };
 
-
+module.exports = mongoose.model('User', UserSchema);
